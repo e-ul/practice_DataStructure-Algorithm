4de47545a3f24ca003973f1465df29313a06035f
e-ul <themmmj382@gmail.com>
2022-02-19 13:04:10 +0900

    shaker sort(bidirection bubble sort)

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,7 +4,8 @@ public class BubbleSort {
 //        bubbleSort(x, 7);
 //        bubbleSortReverse(x, 7);
 //        bubbleSort_v2(x, 7);
-        bubbleSort_v3(x, 7);
+//        bubbleSort_v3(x, 7);
+        bubbleSort_v4(x, 7);
     }
 
     static void bubbleSort(int[] a, int n) {
@@ -64,4 +65,27 @@ public class BubbleSort {
     }
 
     // 06 - Q5 : 양방향 버블정렬 해보기
+    static void bubbleSort_v4(int[] a, int n) {
+        int k_odd = 0;
+        int k_even = n-1;
+        int last = k_even;
+
+        while (k_odd < k_even) {
+            for(int j = k_even ; j > k_odd ; j--) {
+                if(a[j - 1] > a[j]){
+                    swap(a, j - 1, j);
+                    last = j;
+                }
+            }
+            k_odd = last;
+
+            for(int j = k_odd ; j < k_even ; j++) {
+                if(a[j] > a[j+1]){
+                    swap(a, j, j+1);
+                    last = j;
+                }
+            }
+            k_even = last;
+        }
+    }
 }
